8727b8fdceb3298d37a4b6fdf11a3d57f5c79b80
sunsun042505 <108930636+sunsun042505@users.noreply.github.com>
2026-01-02 01:01:46 +0900

    Delete netlify/functions directory

--- netlify/functions/ping.js
+++
@@ -1,6 +0,0 @@
-export default async function handler() {
-  return new Response(JSON.stringify({ ok: true, ts: new Date().toISOString() }), {
-    status: 200,
-    headers: { "content-type": "application/json; charset=utf-8" },
-  });
-}
